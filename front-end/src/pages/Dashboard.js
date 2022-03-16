9d5aa2b6fe4974b169851635c135547256ea55c5
Teyanna Earle <teyonna.earle92@gmail.com>
2022-03-16 17:45:56 -0400

    add dash menu

--- front-end/src/pages/Dashboard.js
+++ front-end/src/pages/Dashboard.js
@@ -1,13 +1,22 @@
-import React from "react"
-import Navbar from "../components/navbar/Navbar"
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+import "./Dashboard.scss";
 
 function Dashboard() {
   return (
     <div>
       <Navbar />
-      <div>Dashboard</div>
+      <div className="dash page">
+        <div className="dash__menu">
+          <a href="#"> Add Medication For Tracking </a>
+          <div className="dash__menu__search">
+            <input placeholder="Search my Meds"></input>
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
